ce4c7e9dc8e5a6ececc3d22675ce4f5d646c7644
Karen Santana <karen.nascimento.developer@gmail.com>
2024-10-02 23:43:38 -0300

    feat(interview): create service and config json server

--- toDoList/src/app/core/services/task.service.ts
+++ toDoList/src/app/core/services/task.service.ts
@@ -1,9 +1,60 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ITask } from '../../shared/interfaces/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private readonly apiUrl = 'http://localhost:3000/tasks';
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+  getTasks(): Observable<ITask[]> {
+    return this.http
+      .get<ITask[]>(this.apiUrl)
+      .pipe(catchError(this.handleError));
+  }
+
+  getTaskById(id: number): Observable<ITask> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<ITask>(url).pipe(catchError(this.handleError));
+  }
+
+  createTask(ITask: ITask): Observable<ITask> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http
+      .post<ITask>(this.apiUrl, ITask, { headers })
+      .pipe(catchError(this.handleError));
+  }
+
+  updateTask(id: number, ITask: ITask): Observable<ITask> {
+    const url = `${this.apiUrl}/${id}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http
+      .put<ITask>(url, ITask, { headers })
+      .pipe(catchError(this.handleError));
+  }
+
+  markTaskAsCompleted(id: number): Observable<ITask> {
+    const url = `${this.apiUrl}/${id}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { isCompleted: true, status: 'done' };
+    return this.http
+      .patch<ITask>(url, body, { headers })
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteTask(id: number): Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<void>(url).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('Ocorreu um erro', error);
+    return throwError(
+      () => new Error('Erro na operação. Por favor, tente novamente.')
+    );
+  }
 }
--- toDoList/src/app/shared/interfaces/task.ts
+++ toDoList/src/app/shared/interfaces/task.ts
@@ -1,2 +1,8 @@
 export interface ITask {
+  id: number;
+  name: string;
+  description: string;
+  status: 'pending' | 'in-progress' | 'done';
+  isCompleted: boolean;
+  date: string;
 }
