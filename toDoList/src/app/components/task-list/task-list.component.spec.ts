e61dd3b8239698b7cc6de4e615cbb8f5a40af0ac
Karen Santana <karen.nascimento.developer@gmail.com>
2024-10-08 08:27:46 -0300

    test(interview): create unit test tasks list and task-modal

--- toDoList/src/app/components/task-list/task-list.component.spec.ts
+++ toDoList/src/app/components/task-list/task-list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskListComponent } from './task-list.component';
 import { getTasksMock } from '../../shared/mock-unit-test/response.mock';
+import { By } from '@angular/platform-browser';
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
@@ -21,5 +22,43 @@ describe('TaskListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should show item list when tasks list lenght is greater than 1", () => {
+    component.tasksList = getTasksMock;
+    fixture.detectChanges();
+    const item = fixture.debugElement.queryAll(By.css('[data-testid="item"]'));
+    const emptyWarning = fixture.debugElement.query(By.css('[data-testid="empty-warning"]'));
+    expect(item.length).toEqual(3);
+    expect(emptyWarning).toBeFalsy();
+  });
+
+  it("should hidden item and show message empty when tasks list to be empty", () => {
+    component.tasksList = [];
+    fixture.detectChanges();
+    const item = fixture.debugElement.queryAll(By.css('[data-testid="item"]'));
+    const emptyWarning = fixture.debugElement.queryAll(By.css('[data-testid="empty-warning"]'));
+    expect(item.length).toEqual(0);
+    expect(emptyWarning).toBeTruthy();
+  });
+
+  it("should emit id when $deleteTask to call", () => {
+    component.tasksList = getTasksMock;
+    const spy = spyOn(component.$deleteTask, 'emit');
+    fixture.detectChanges();
+    const item = fixture.debugElement.query(By.css('[data-testid="item"]'));
+    item.triggerEventHandler('$deleteTask');
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith('734cb163-e2cd-4801-b7f8-dda12b792114' );
+  });
+
+  it("should emit item when $editTask to call", () => {
+    component.tasksList = getTasksMock;
+    const spy = spyOn(component.$editTask, 'emit');
+    fixture.detectChanges();
+    const item = fixture.debugElement.query(By.css('[data-testid="item"]'));
+    item.triggerEventHandler('$editTask', getTasksMock[0]);
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(getTasksMock[0]);
   });
 });
--- toDoList/src/app/components/task-modal/task-modal.component.spec.ts
+++ toDoList/src/app/components/task-modal/task-modal.component.spec.ts
@@ -1,23 +1,87 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskModalComponent } from './task-modal.component';
+import { By } from '@angular/platform-browser';
+
+function testPrimaryButton(): void {
+  console.log('Primary button clicked');
+}
+
+function testSecondaryButton(): void {
+  console.log('Secondary button clicked');
+}
 
 describe('TaskModalComponent', () => {
   let component: TaskModalComponent;
   let fixture: ComponentFixture<TaskModalComponent>;
+  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskModalComponent]
-    })
-    .compileComponents();
+      imports: [TaskModalComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TaskModalComponent);
     component = fixture.componentInstance;
+    component.actions = {
+      primary: {
+        label: 'Continuar',
+        callback: jasmine.createSpy('primaryCallback')
+      },
+      secondary: {
+        label: 'Cancelar',
+        callback: jasmine.createSpy('primaryCallback')
+      },
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show modal when openModal to equal true', () => {
+    component.openModal = true;
+    fixture.detectChanges();
+    const modal = fixture.debugElement.query(By.css('[data-testid="modal"]'));
+    const overlay = fixture.debugElement.query(
+      By.css('[data-testid="overlay"]')
+    );
+    expect(modal).toBeTruthy();
+    expect(overlay).toBeTruthy();
+  });
+
+  it('should hidden modal when openModal to equal true', () => {
+    component.openModal = false;
+    fixture.detectChanges();
+    const modal = fixture.debugElement.query(By.css('[data-testid="modal"]'));
+    const overlay = fixture.debugElement.query(
+      By.css('[data-testid="overlay"]')
+    );
+    expect(modal).toBeFalsy();
+    expect(overlay).toBeFalsy();
+  });
+
+  it('should callback primary when click in primary button', () => {
+    component.openModal = true;
+    fixture.detectChanges();
+    const primaryButton = fixture.debugElement.query(
+      By.css('[data-testid="primary-button"]')
+    );
+    primaryButton.triggerEventHandler('click');
+    fixture.detectChanges();
+    expect(component.actions.primary.callback).toHaveBeenCalled();
+  });
+
+  it('should callback secondary when click in secondary button', () => {
+    component.openModal = true;
+    fixture.detectChanges();
+    const primaryButton = fixture.debugElement.query(
+      By.css('[data-testid="secondary-button"]')
+    );
+    primaryButton.triggerEventHandler('click');
+    fixture.detectChanges();
+    expect(component.actions.secondary.callback).toHaveBeenCalled();
+  });
 });
+
